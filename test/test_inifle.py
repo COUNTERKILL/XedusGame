2eb0f822110f0707733aaa56f5bd3c97926225de
COUNTERKILL <COUNTERKILL@inbox.ru>
2015-10-24 21:44:06 +0500

    Modified .travis.yml

--- test/test_inifle.py
+++ test/test_inifle.py
@@ -7,7 +7,7 @@ class Test(unittest.TestCase):
 	def testWriteToIni(self):
 		iniFile = IniFile("config.ini")
 		iniFile.Write("config", "Name", "Oleg")
-		self.assertEqual(iniFile.Read("config", "Name"), "Oleg")
+		self.assertEqual(iniFile.ReadString("config", "Name"), "Oleg")
 
 if __name__ == "__main__":
    unittest.main()
