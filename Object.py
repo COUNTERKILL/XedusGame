50840420b705d5b90a61d59a9262eb3678a69b30
Pangea7 <Sociopatdnd@gmail.com>
2015-10-24 22:47:24 +0500

    Class which represents object in a game

---
+++ Object.py
@@ -0,0 +1,42 @@
+import sfml as sf
+
+TYPE_INTERACTION_PHYSICAL=0
+TYPE__INTERACTION_VISUAL=1
+TYPE__LOGIC_IMPASSABLE_AREA=0
+TYPE_LOGIC_PASSABLE_AREA=1
+TYPE_LOGIC_ANOMALY=2
+TYPE_LOGIC_ARTEFACT=3
+TYPE_LOGIC_NPC=4
+
+
+class Position:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+    def __str__(self):
+        strx=str(self.x)
+        stry=str(self.y)
+        return strx+' '+stry
+        
+class Object(sf.Drawable):
+    def __init__(self,texture,width,height):
+        self.width=width
+        self.height=height
+        self.texture=texture
+        sf.Drawable.__init__(self)
+    position=Position(0,0)
+    obj_id=None
+    type_interatction=None
+    type_logic=None
+    texture=sf.Texture
+    def setposition(self,pos):
+        self.position=pos;
+    def getposition(self):
+        return self.position
+texture = sf.Texture.from_file("D:\Downloads\image.jpg")
+m1=Object(texture,450,450)
+m2=Position(4,3)
+m1.setposition(m2)
+print(m1.position)
+print TYPE_PHYSIC
+
