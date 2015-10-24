6181ad65be9514dd8975a2b05d5750f41b4410e6
KobyakOleg <KobyakgitHub@yandex.ru>
2015-10-24 20:12:48 +0600

    Updated with reading Int and Bool, added function to write new parametrs.

--- IniFile.py
+++ IniFile.py
@@ -1,22 +1,71 @@
+import os
+
 class IniFile:
 	def __init__(self, fileName):
 		self._fileName = fileName
+
 	def ReadString(self, sectionName, paramName):
-		X=sectionName
-		Y=paramName
-		stroka='['+X+']'
-		file=open(self._fileName,'r')
+		section='['+sectionName+']'
 		sectionFinded = False
-		for line in file:
-			if stroka in line:
-				sectionFinded = True
-				continue
-			if sectionFinded == True:			
-				if line[0]=='[':
-					return
-				else:
-					if Y in line:
-						index_res_start=line.find('=')+1
-						result=line[index_res_start:len(line)]
-						return result					
+		with open(self._fileName,'r') as file:
+			for line in file:
+				if section in line:
+					sectionFinded = True
+					continue
+				if sectionFinded == True:			
+					if line[0]=='[':
+						return
+					else:
+						if paramName in line:
+							index_param_start=line.find('=')+1
+							value=line[index_param_start:len(line)]
+							return value
+		
 	_fileName=None
+
+	def ReadInt(self,sectionName,paramName):
+		value = IniFile.ReadString(self,sectionName,paramName)
+		value = int(value)
+		return value
+		
+	def ReadBool(self,sectionName,paramName):
+		value = IniFile.ReadString(self,sectionName,paramName)
+		value = bool(value)
+		return value
+		
+	def Write(self,sectionName,paramName,paramValue):
+		section='['+sectionName+']'
+		sectionFinded = False
+		copied = False
+		with open(self._fileName,'r+') as file:
+			with open('Copy_'+self._fileName,'w') as fileCopy:
+				for line in file:
+					if copied == False:
+						if section in line:
+							sectionFinded = True
+							fileCopy.write(line)
+							continue
+						if sectionFinded == True:			
+							if line[0]=='[':
+								fileCopy.write(paramName+'='+paramValue+'\n')
+								fileCopy.write(line)
+								copied = True
+							elif ((paramName+'=') in line) & (paramName[0]==line[0]):
+								fileCopy.write(paramName+'='+paramValue+'\n')
+								copied = True
+								continue
+							else:
+								fileCopy.write(line)
+						else:
+							fileCopy.write(line)
+							continue		
+					else:
+						fileCopy.write(line)
+				if sectionFinded == False:
+					fileCopy.write('\n'+section)
+					fileCopy.write('\n'+paramName+'='+paramValue+'\n')
+				else:
+					if copied == False:
+						fileCopy.write('\n'+paramName+'='+paramValue)
+		os.remove(fileName)
+		os.rename('Copy_'+fileName,fileName)
