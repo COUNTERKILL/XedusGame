3bc17c511dc2d77dc4898608c37111360eed76d7
Pangea7 <Sociopatdnd@gmail.com>
2015-11-13 00:44:24 +0500

    Animation object debugged

--- AnimationObject.py
+++ AnimationObject.py
@@ -9,19 +9,22 @@ class AnimationObject(obj.Object):
         obj.Object.__init__(self,ObjIniPath)
         load=ini.IniFile(ObjIniPath)
         animation_count=load.ReadInt("animation","count")
-        for j in range(0, animations_count):
+        for j in range(0, animation_count):
             frames_count=load.ReadInt("animation"+str(j),"frames_count")
-            animation=ani.Animation(load.ReadInt("animation","frame_time"))
-            animation.frame_width=load.ReadInt("animation","frames_width")
-            animation.frame_height=load.ReadInt("animation","frames_height")
+            animation=ani.Animation(load.ReadFloat("animation"+str(j),"frame_time"))
+            animation.frame_width=load.ReadInt("animation","frame_width")
+            animation.frame_height=load.ReadInt("animation","frame_height")
             animation.SetSpriteTexture(self.sprite)
             for i in range(0, frames_count):
                 animation.AddFrame(sf.Rectangle
-                (sf.Vector2(load.ReadInt("frame"+str(i),pos_x),
-                                        load.ReadInt("frame"+str(i),pos_y)),
+                (sf.Vector2(load.ReadInt("frame"+str(i),"pos_x"),
+                                        load.ReadInt("frame"+str(i),"pos_y")),
                              sf.Vector2(animation.frame_width,animation.frame_height)))
                 self.Animations[load.ReadString("animation"+str(j),"name")]=animation
     Animations={}
+if __name__=="__main__":
+    anim=AnimationObject("C:\\Users\\User\\Desktop\\Repository\\Repository\\locations\\test_loc\\objects\\test_obj_animated_electra.ini")
+
                 
 
 
