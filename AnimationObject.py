c5e8666f5e66cccb5175d0bce121baf3a59cbd67
Pangea7 <Sociopatdnd@gmail.com>
2015-11-12 23:07:59 +0500

    Added names for animations

--- AnimationObject.py
+++ AnimationObject.py
@@ -11,16 +11,18 @@ class AnimationObject(obj.Object):
         animation_count=load.ReadInt("animation","count")
         for j in range(0, animations_count):
             frames_count=load.ReadInt("animation"+str(j),"frames_count")
-            animation=ani(load.ReadInt("animation","frames_time")
+            animation=ani.Animation(load.ReadInt("animation","frame_time"))
             animation.frame_width=load.ReadInt("animation","frames_width")
             animation.frame_height=load.ReadInt("animation","frames_height")
             animation.SetSpriteTexture(self.sprite)
             for i in range(0, frames_count):
-			animation.AddFrame(sf.Rectangle
-                                           (sf.Vector2(load.ReadInt("frame"+str(i),pos_x),load.ReadInt("frame"+str(i),pos_y)),
-                                            sf.Vector2(animation.frame_width,animation.frame_height)))
-            self.Animations["animation"+str(j)]=animation
+                animation.AddFrame(sf.Rectangle
+                (sf.Vector2(load.ReadInt("frame"+str(i),pos_x),
+                                        load.ReadInt("frame"+str(i),pos_y)),
+                             sf.Vector2(animation.frame_width,animation.frame_height)))
+                self.Animations[load.ReadString("animation"+str(j),"name")]=animation
     Animations={}
+                
 
 
 
