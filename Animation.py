fadb88fc93198f64175c0628f1cffa4bdece9b53
unknown <prikazchikov@PC1121.class.local>
2015-11-13 16:42:01 +0500

    Fixed AnimatedObject

--- Animation.py
+++ Animation.py
@@ -1,34 +1,33 @@
 import sfml as sf
 import time
 class Animation:
-    def __init__(self,framestime,name):
-        self.framestime=sf.seconds(framestime)
-        self.clock=sf.Clock()
-        self.currentframe=0
-        self.name=name
-    def AddFrame(self,rect):
-        self.frames.append(rect)
-    def GetFrame(self):
-        if self.clock.elapsed_time>self.framestime:
-            self.clock.restart()
-            self.currentframe+=1
-            if self.currentframe==len(self.frames):
-                self.currentframe=0
-                return self.frames[self.currentframe]
-            return self.frames[self.currentframe]
-        else:
-            self.clock.restart()
-            return self.frames[self.currentframe]
-    def SetSpriteTexture(self,texture):
-        self.texture=texture
-    def GetSpriteTexture(self):
-        return self.texture
-    name=None
-    framestime=None
-    clock=None
-    frames=[]
-    currentframe=None
-    texture=None
+	def __init__(self,framestime,name):
+		self.framestime=sf.seconds(framestime)
+		self.clock=sf.Clock()
+		self.currentframe=0
+		self.name=name
+	def AddFrame(self,rect):
+		self.frames.append(rect)
+	def GetFrame(self):
+		if self.clock.elapsed_time>self.framestime:
+			self.clock.restart()
+			self.currentframe+=1
+			if self.currentframe==len(self.frames):
+				self.currentframe=0
+				return self.frames[self.currentframe]
+			return self.frames[self.currentframe]
+		else:
+			return self.frames[self.currentframe]
+	def SetSpriteTexture(self,texture):
+		self.texture=texture
+	def GetSpriteTexture(self):
+		return self.texture
+	name=None
+	framestime=None
+	clock=None
+	frames=[]
+	currentframe=None
+	texture=None
 
 
 if __name__=="__main__":
