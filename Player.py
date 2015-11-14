da074dbc4611bc9d1501838951b93279f82f3873
Pangea7 <Sociopatdnd@gmail.com>
2015-11-14 16:47:06 +0500

    Added position in Collide function, Created class Player, Created Ini File for Player

---
+++ Player.py
@@ -0,0 +1,23 @@
+import AnimatedObject as ao
+import Location as loc
+import Physics as Phys
+import IniFile as ini
+import Object as obj
+class Player(ao.AnimatedObject):
+    def __init__(self,IniPath):
+        ao.AnimatedObject.__init__(self,IniPath)
+        load=ini.IniFile(IniPath)
+        self._location=loc.Location(load.ReadString("player","location"))
+    def Move(self,step):
+        valid=True
+        for obj in self._location.objects:
+            print(Phys.Collide(self,obj))
+            if Phys.Collide(self,obj)==True:
+                valid=False
+        if(valid):
+            self.position.x=step.x
+            self.position.y=step.y
+    def SetLocation(self,location):
+        self._location=location
+
+        
