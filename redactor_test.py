70f79a1ede979aa2250236d662c9ecede5767254
COUNTERKILL <COUNTERKILL@inbox.ru>
2015-11-15 16:07:04 +0500

    Fixed recreate object with add in test_redactor

--- redactor_test.py
+++ redactor_test.py
@@ -14,19 +14,15 @@ loc = Location("locations\\test_loc\\redactor_loc.ini")
 # start the game loop
 obj=None
 a=False
-context=False
 while window.is_open:
     # process events
     for event in window.events:
         # close window: exit
 		if type(event) is sf.CloseEvent:
 			window.close()
-		if sf.Keyboard.is_key_pressed(sf.Keyboard.A) or context:
+		if sf.Keyboard.is_key_pressed(sf.Keyboard.A):
 			obj=AnimatedObject.AnimatedObject("locations\\test_loc\\objects\\test_obj_animated_electra.ini")
-			position=sf.Mouse.get_position()
-			obj.SetPosition(Position(position.x,position.y))
 			a=True
-			context=True
 		if sf.Keyboard.is_key_pressed(sf.Keyboard.RETURN):
 			if obj:
 				loc.AddObject(obj, 0)
@@ -35,7 +31,6 @@ while window.is_open:
 			obj=None
 		if sf.Keyboard.is_key_pressed(sf.Keyboard.ESCAPE):
 			a=False
-			context=False
 			obj=None
 		if sf.Keyboard.is_key_pressed(sf.Keyboard.RIGHT):
 			step.x=Player.STEP_SIZE
@@ -43,6 +38,9 @@ while window.is_open:
 			step.y=-1*Player.STEP_SIZE
 		if sf.Keyboard.is_key_pressed(sf.Keyboard.DOWN):
 			step.y=Player.STEP_SIZE
+		if obj:
+			position=sf.Mouse.get_position()
+			obj.SetPosition(Position(position.x,position.y))
     view.center = sf.Mouse.get_position()
     window.clear() # clear screen
     window.draw(loc) # draw the sprite
