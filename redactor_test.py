8ed88b5bda94f1882e3c66c7cf96810ea15cc20b
COUNTERKILL <COUNTERKILL@inbox.ru>
2015-11-15 15:52:47 +0500

    Fixed redactor_test

--- redactor_test.py
+++ redactor_test.py
@@ -19,29 +19,30 @@ while window.is_open:
     # process events
     for event in window.events:
         # close window: exit
-        if type(event) is sf.CloseEvent:
-            window.close()
-        if sf.Keyboard.is_key_pressed(sf.Keyboard.A) or context:
-            obj=AnimatedObject.AnimatedObject("locations\\test_loc\\objects\\test_obj_animated_electra.ini")
-            position=sf.Mouse.get_position()
-            obj.SetPosition(Position(position.x,position.y))
-            a=True
-            context=True
-            if sf.Keyboard.is_key_pressed(sf.Keyboard.RETURN):
-                loc.AddObject(obj,0)
-                a=False
-                context=False
-                obj=None
-            if sf.Keyboard.is_key_pressed(sf.Keyboard.ESCAPE):
-                a=False
-                context=False
-                obj=None
-        if sf.Keyboard.is_key_pressed(sf.Keyboard.RIGHT):
-            step.x=Player.STEP_SIZE
-        if sf.Keyboard.is_key_pressed(sf.Keyboard.UP):
-            step.y=-1*Player.STEP_SIZE
-        if sf.Keyboard.is_key_pressed(sf.Keyboard.DOWN):
-            step.y=Player.STEP_SIZE
+		if type(event) is sf.CloseEvent:
+			window.close()
+		if sf.Keyboard.is_key_pressed(sf.Keyboard.A) or context:
+			obj=AnimatedObject.AnimatedObject("locations\\test_loc\\objects\\test_obj_animated_electra.ini")
+			position=sf.Mouse.get_position()
+			obj.SetPosition(Position(position.x,position.y))
+			a=True
+			context=True
+		if sf.Keyboard.is_key_pressed(sf.Keyboard.RETURN):
+			if obj:
+				loc.AddObject(obj, 0)
+			a=False
+			context=False
+			obj=None
+		if sf.Keyboard.is_key_pressed(sf.Keyboard.ESCAPE):
+			a=False
+			context=False
+			obj=None
+		if sf.Keyboard.is_key_pressed(sf.Keyboard.RIGHT):
+			step.x=Player.STEP_SIZE
+		if sf.Keyboard.is_key_pressed(sf.Keyboard.UP):
+			step.y=-1*Player.STEP_SIZE
+		if sf.Keyboard.is_key_pressed(sf.Keyboard.DOWN):
+			step.y=Player.STEP_SIZE
     view.center = sf.Mouse.get_position()
     window.clear() # clear screen
     window.draw(loc) # draw the sprite
